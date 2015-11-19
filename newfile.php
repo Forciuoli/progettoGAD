f1f970b47cee6a0f008c07b2f7ba67cb4451077b
dConcilio <tarx@hotmail.it>
2015-11-19 18:27:21 +0100

    link extract wikipedia 2010/2011

--- newfile.php
+++ newfile.php
@@ -1,9 +1,18 @@
 <?php
 
-$ch = curl_init("https://api.import.io/store/data/ad239e5f-a9ce-487c-98ae-d374dc1d9c13/_query?input/webpage/url=http%3A%2F%2Fwww.gamerevolution.com%2Fgame%2Fall%2F1%2Flong_name%2Fasc&_user=12c26aee-8ae3-4b58-af08-e4df583faf99&_apikey=12c26aee8ae34b58af08e4df583faf9998be34fe53a13dfaa52cf5ddf1659d6a7b653a5b9635b9a5163de392f0cd19b9aee504b936fc41c39753801434669d86b936fd19499a91ddee477b8c5196a326");
+$ch = curl_init("https://api.import.io/store/data/800fe53d-9a73-4e88-b598-a6e6e2897012/_query?input/webpage/url=http%3A%2F%2Fwww.gamerevolution.com%2Fgame%2Fall%2Fa%2Flong_name%2Fasc&_user=1612660c-6d35-44b0-bf1d-29a49efd169b&_apikey=1612660c6d3544b0bf1d29a49efd169bf68f20bae1b1e7fe100d0c943b328a0b9266dedd030dd5c9f87c9863938967c52c8d7be1b9d2674cfd6318083e289aa38f29f192f864849a7d6e7341951a47ef");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 curl_close($ch);
+//echo $result;
   $someObject = json_decode($result);
-  echo $someObject->results[0]->pepo;
+  for ($i=0;$i < count($someObject->results);$i++)
+  {
+  	echo $someObject->results[$i]->name.'<br/>';
+  }
+  
+  //genre and publisher
+  //https://api.import.io/store/data/13daeba6-655e-4af9-93b3-34e89dcf842a/_query?input/webpage/url=http%3A%2F%2Fwww.gamerevolution.com%2Fgame%2F0-day-attack-on-earth&_user=1612660c-6d35-44b0-bf1d-29a49efd169b&_apikey=1612660c6d3544b0bf1d29a49efd169bf68f20bae1b1e7fe100d0c943b328a0b9266dedd030dd5c9f87c9863938967c52c8d7be1b9d2674cfd6318083e289aa38f29f192f864849a7d6e7341951a47ef
 
+//link wipedia 2010/2011
+//https://api.import.io/store/data/ced00686-b169-4950-8400-734195ed62a1/_query?input/webpage/url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2F2010_in_video_gaming&_user=1612660c-6d35-44b0-bf1d-29a49efd169b&_apikey=1612660c6d3544b0bf1d29a49efd169bf68f20bae1b1e7fe100d0c943b328a0b9266dedd030dd5c9f87c9863938967c52c8d7be1b9d2674cfd6318083e289aa38f29f192f864849a7d6e7341951a47ef
