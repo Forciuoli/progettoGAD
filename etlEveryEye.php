606a74f3ef75dc14c997c86b593bca7ae0c50d42
Donato <tarx@hotmail.it>
2015-12-07 18:37:02 +0100

    c

--- etlEveryEye.php
+++ etlEveryEye.php
@@ -1,7 +1,7 @@
 <?php
 //require "classGame.php";
 function getGameEveryEye(){
-set_time_limit(0);
+set_time_limit(0); 
 $months = ["gennaio"];//,"aprile","maggio","giugno","luglio","agosto","settembre","ottobre","novembre","dicembre"];
  for ($j = 0; $j < count($months); $j++) 
  {
